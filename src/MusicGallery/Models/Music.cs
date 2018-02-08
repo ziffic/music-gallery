f5485d62bd95706ff2fcb484345d4ae06795cdea
David Yrigoyen <dave@dryweb.com>
2018-02-07 22:47:43 -0800

    Added music and artist models

---
+++ src/MusicGallery/Models/Music.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicGallery.Models
+{
+  public class Music
+  {
+    public int Id { get; set; }
+    public string BandName { get; set; }
+    public string AlbumTitle { get; set; }
+    public string ReleaseDate { get; set; }
+    public string DescriptionHtml { get; set; }
+    public Artist[] Artists { get; set; }
+  }
+}
