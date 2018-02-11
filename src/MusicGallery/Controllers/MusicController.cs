9f5a7d2fb77718fade960f09082ddfa8ad45dbe1
David Yrigoyen <dave@dryweb.com>
2018-02-11 13:14:49 -0800

    Updated Music Detail view to be strongly typed

--- src/MusicGallery/Controllers/MusicController.cs
+++ src/MusicGallery/Controllers/MusicController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MusicGallery.Models;
 
 namespace MusicGallery.Controllers
 {
@@ -10,19 +11,22 @@ namespace MusicGallery.Controllers
   {
     public ActionResult Detail()
     {
-      ViewBag.BandName = "Tool";
-      ViewBag.AlbumTitle = "Lateralus";
-      ViewBag.ReleaseDate = "May 15, 2001";
-      ViewBag.Description = "<p>Lateralus is the third studio album by American rock band Tool. It was released through <strong>Volcano Entertainment</strong>.</p>";
-      ViewBag.Artists = new string[]
+      var music = new Music()
       {
-        "Vocals: Maynard James Keenan",
-        "Guitar: Adam Jones",
-        "Bass: Justin Chancellor",
-        "Drums: Danny Carey"
+        BandName = "Tool",
+        AlbumTitle = "Lateralus",
+        ReleaseDate = "May 15, 2001",
+        DescriptionHtml = "<p>Lateralus is the third studio album by American rock band Tool. It was released through <strong>Volcano Entertainment</strong>.</p>",
+        Artists = new Artist[]
+        {
+          new Artist() { Name = "Maynard James Keenan", Role = "Vocals" },
+          new Artist() { Name = "Adam Jones", Role = "Guitar" },
+          new Artist() { Name = "Justin Chancellor", Role = "Bass" },
+          new Artist() { Name = "Danny Carey", Role = "Drums" }
+        }
       };
 
-      return View();
+      return View(music);
     }
   }
 }
